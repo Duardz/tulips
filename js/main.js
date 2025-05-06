7d81afca629c5316936495463b621cc8e1db20f8
duard <duard@dev.com>
2025-05-06 16:04:17 +1200

    Typing animation on Landing page

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function checkAnswer() {
     const feedbackElement = document.getElementById('feedback');
     
     if (userInput === correctAnswer) {
-      feedbackElement.textContent = ";)";
+      feedbackElement.textContent = "Correct! Redirecting to your poem...";
       feedbackElement.style.color = "#64a338";
       
       // Add a little animation before redirecting
